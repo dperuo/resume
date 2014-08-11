ddca7e6575d4f18a1ea8aa388124f944689bd9dd
dperuo <derek@dbpventures.com>
2014-08-11 11:48:44 -0400

    ng-repeat for skills

--- js/resume.js
+++ js/resume.js
@@ -2,6 +2,48 @@
 (function() {
   angular.module('resumeApp', ['ngTouch']).controller('headerRowCtrl', function() {
     return this.toggleContactInfo = false;
+  }).controller('skillSelector', function() {
+    this.catagory = "Top 12";
+    this.skillList = [
+      {
+        name: "Angular.js",
+        cat: "Top 12"
+      }, {
+        name: "Bootstrap 3",
+        cat: "Top 12"
+      }, {
+        name: "CoffeeScript",
+        cat: "Top 12"
+      }, {
+        name: "Compass",
+        cat: "Top 12"
+      }, {
+        name: "CSS3",
+        cat: "Top 12"
+      }, {
+        name: "Foundation 5",
+        cat: "Top 12"
+      }, {
+        name: "HTML5",
+        cat: "Top 12"
+      }, {
+        name: "Illustrator",
+        cat: "Top 12"
+      }, {
+        name: "jQuery",
+        cat: "Top 12"
+      }, {
+        name: "Less",
+        cat: "Top 12"
+      }, {
+        name: "Photoshop",
+        cat: "Top 12"
+      }, {
+        name: "Sass",
+        cat: "Top 12"
+      }
+    ];
+    return true;
   });
 
   $(function() {
