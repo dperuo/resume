cbcb80396a40984505196d8f71be786bbcc4d256
dperuo <derek@dbpventures.com>
2014-08-12 11:05:10 -0400

    changes

--- _resume2/gulpfile.js
+++ _resume2/gulpfile.js
@@ -1,9 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass');
 
+
+
+// --------------- PATHS --------------- //
 var inputs = {
-  coffee: 'src/coffee/**/*.coffee',
-  img:    'src/img/**/*',
-  scss:   'src/scss/**/*.scss'
+  img:  'src/img/**/*',
+  scss: 'src/scss/**/*.scss',
+  ts:   'src/coffee/**/*.ts'
 };
 
 var outputs = {
@@ -12,6 +16,19 @@ var outputs = {
   js:  'app/js'
 };
 
-gulp.task('default', [], function() {
-  console.log(inputs, outputs)
+
+
+// --------------- TASKS --------------- //
+gulp.task('scss', function () {
+  gulp.src(inputs.scss)
+    .pipe(sass({style: 'compressed'}))
+    .pipe(gulp.dest(outputs.css))
+});
+
+// gulp.task('default', ['scss'], function() {
+//   gulp.watch(inputs.scss, ['scss'])
+// });
+
+gulp.task('default', function () {
+  console.log (inputs, outputs);
 });
